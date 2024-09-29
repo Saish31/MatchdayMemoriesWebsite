3c15ee4c8cd534ff5dc8fff01d9c8551af12c36d
Saish31 <139218429+Saish31@users.noreply.github.com>
2024-09-29 22:23:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ def show_post(post_id):
 
 # Use a decorator so only an admin user can create new posts
 @app.route("/new-post", methods=["GET", "POST"])
-@admin_only
+#@admin_only
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
